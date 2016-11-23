c06848fb441993b02159d8f34df3f182f4ca2521
The-Tesla <wifak.anass@gmail.com>
2016-11-23 21:37:49 +0000

    added beta in diff equation au''+bu=f

--- Main.m
+++ Main.m
@@ -3,13 +3,21 @@ clear
 clc
 
 a=0;
-b=pi ;
+b=1;
 h=0.1;
 alpha=1;
-ue=@(x)sin(x);
-f=@(x)sin(x);
+beta=1;
+p1 =     -0.1081  ;
+p2 =      0.1285  ;
+p3 =    -0.06277  ;
+p4 =     0.01778  ;
+p5 =     0.02459  ;
+p6 =   4.258e-05 ;
+
+ue=@(x)p1*x^5 + p2*x^4 + p3*x^3 + p4*x^2 + p5*x + p6;
+f=@(x)x^4;
 [X, T] = MaillageP1(a, b, h);
-[U] = EF_P1(alpha,f,X,T);
+[U] = EF_P1(alpha,beta,f,X,T);
 % Post-traitement
 %%%%%%   comparaison graphique
 figure('name', 'Comparaision: solutions exacte et approchée '); fplot(ue, [min(X), max(X)], 'b'); hold on; plot(X,U, 'r.-');
@@ -18,6 +26,7 @@ erreur=zeros(size(X,1),1);
 for i=1:size(X,1)
 erreur(i)=U(i)-ue(i);
 end
-figure('name', 'analyse erreur'); plot(X,erreur, 'r-o');
+figure('name', 'analyse erreur'); 
+plot(X,erreur, 'r-o');
 ylabel('erreur');
 xlabel('noeuds');
--- mat_elem_P1.m
+++ mat_elem_P1.m
@@ -1,4 +1,4 @@
-function [ elemKi ] = mat_elem_P1(alpha,X,T,i)
+function [ elemi ] = mat_elem_P1(alpha,beta,X,T,i)
 %calcule la matrice élémentaire dans l'élément Ti
 %               Ti
 %       |---------------|
@@ -11,7 +11,15 @@ xm=(x2+x1)*0.5;
 A=(x2-x1)/6*(d_phi1(x1,x1,x2)^2+4*d_phi1(xm,x1,x2)^2+d_phi1(x2,x1,x2)^2);
 B=(x2-x1)/6*(d_phi1(x1,x1,x2)*d_phi2(x2,x1,x2)+4*d_phi1(xm,x1,x2)*d_phi2(x2,x1,x2)+d_phi1(x2,x1,x2)*d_phi2(x2,x1,x2));
 C=(x2-x1)/6*(d_phi2(x1,x1,x2)^2+4*d_phi2(xm,x1,x2)^2+d_phi2(x2,x1,x2)^2);
-elemKi=[A  B;B   C];
+elemKi=alpha*[A B;B C];
+
+A=(x2-x1)/6*(phi1_P1(x1,x1,x2)^2+4*phi1_P1(xm,x1,x2)^2+phi1_P1(x2,x1,x2)^2);
+B=(x2-x1)/6*(phi1_P1(x1,x1,x2)*phi2_P1(x2,x1,x2)+4*phi1_P1(xm,x1,x2)*phi2_P1(x2,x1,x2)+phi1_P1(x2,x1,x2)*phi2_P1(x2,x1,x2));
+C=(x2-x1)/6*(phi2_P1(x1,x1,x2)^2+4*phi2_P1(xm,x1,x2)^2+phi2_P1(x2,x1,x2)^2);
+elemMi=beta*[A B;B C];
+% elemMi=beta*((x2-x1)/6)*[2 1;1 2];
+elemi=elemMi+elemKi;
+
 %elemKi=(alpha/(x2-x1))*[1  -1;-1   1];
 
 end
